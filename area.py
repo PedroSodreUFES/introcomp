56657cee3acc8e778b3fa41cba7bf5a5e9528d2b
PedroSodreUFES <pedro.malini@edu.ufes.br>
2024-08-20 02:18:30 -0300

    fresh start

--- area.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Area(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int):
-        super().__init__
-        self.rect = pygame.Rect(x, y, 120, 90)
-    def drawArea(self, janela, cor):
-        pygame.draw.rect(janela, cor, self.rect)
